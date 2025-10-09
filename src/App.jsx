0837a221c8017edfd734c47b6bd8b1434805f9dd
gilmerperez <gilmer2002@outlook.com>
2025-10-09 19:43:14 -0400

    Formatted json package lock, cleaned up global index css and added root variables, cleaned up App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
+import Header from "./components/Header/Header";
 import Home from "./pages/Home";
 import Movies from "./pages/Movies";
+import TVShows from "./pages/TVShows";
 import TopIMDB from "./pages/TopIMDB";
 import Contact from "./pages/Contact";
-import TVShows from "./pages/TVShows";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import TermsOfService from "./pages/TermsOfService";
 import MediaDetail from "./components/MediaDetail/MediaDetail";
-import { BrowserRouter as Router, Routes, Route, Navigate, useParams } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+import { BrowserRouter as useParams, Navigate, Router, Routes, Route } from "react-router-dom";
 
-// Redirect Top IMDB detail routes
+// * Redirect Top IMDB detail routes
 function RedirectTopIMDB() {
   const { id } = useParams();
   return <Navigate to={`/movie/${id}`} replace />;
@@ -22,7 +22,6 @@ function App() {
       <Router>
         <Header />
         <Routes>
-          {/* Static Pages */}
           <Route path="/" element={<Home />} />
           <Route path="/movies" element={<Movies />} />
           <Route path="/tv-shows" element={<TVShows />} />
@@ -30,7 +29,6 @@ function App() {
           <Route path="/contact" element={<Contact />} />
           <Route path="/privacy-policy" element={<PrivacyPolicy />} />
           <Route path="/terms-of-service" element={<TermsOfService />} />
-          {/* Dynamic Detail Pages */}
           <Route path="/movie/:id" element={<MediaDetail mediaType="movie" />} />
           <Route path="/tv-show/:id" element={<MediaDetail mediaType="tv" />} />
           <Route path="/top-imdb/:id" element={<RedirectTopIMDB />} />
