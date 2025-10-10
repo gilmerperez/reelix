cc60e15ef21616c770793539c825ad41c9a6837e
gilmerperez <gilmer2002@outlook.com>
2025-10-09 20:18:58 -0400

    Optimized footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,25 +3,32 @@ import styles from "./Footer.module.css";
 
 function Footer() {
   return (
-    <footer className={styles.footer}>
-      <div className={`container ${styles.container}`}>
-        {/* Navigation Links */}
-        <nav className={styles.links}>
-          <Link to="/">Home</Link>
-          <Link to="/contact">Contact</Link>
-          <Link to="/privacy-policy">Privacy Policy</Link>
-          <Link to="/terms-of-service">Terms of Service</Link>
-        </nav>
-        {/* Legal Disclaimer */}
-        <p className={styles.description}>
-          Reelix is a Movie and TV Show database. It does not host, stream, or distribute any copyrighted content. All
-          metadata and media previews are provided via public APIs. No user accounts, payments, or downloads are
-          required.
-        </p>
-        {/* Copyright */}
-        <p className={styles.copy}>&copy; {new Date().getFullYear()} Reelix</p>
-      </div>
-    </footer>
+    <>
+      <footer>
+        <div className={styles.footerContainer}>
+          {/* Legal disclaimer */}
+          <p className={styles.legalDisclaimer}>
+            Reelix is a Movie and TV Show database. It does not host, stream, or distribute any copyrighted content. All
+            metadata and media previews are provided via public APIs. No user accounts, payments, or downloads are
+            required.
+          </p>
+          {/* Copyright */}
+          <p className={styles.copyright}>&copy; {new Date().getFullYear()} Reelix. All rights reserved.</p>
+          {/* Seperator */}
+          <hr className={styles.seperator} />
+          {/* Navigation links */}
+          <nav className={styles.siteNavigation}>
+            <Link to="/">Home</Link>
+            <Link to="/movies">Movies</Link>
+            <Link to="/tv-shows">TV Shows</Link>
+            <Link to="/top-imdb">Top IMDB</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/privacy-policy">Privacy Policy</Link>
+            <Link to="/terms-of-service">Terms of Service</Link>
+          </nav>
+        </div>
+      </footer>
+    </>
   );
 }
 
