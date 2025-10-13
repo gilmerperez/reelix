646d20134ae572e1b7fe7cfde3a3c0c97aa97bad
gilmerperez <gilmer2002@outlook.com>
2025-10-13 15:33:26 -0400

    Removed info section

--- src/components/MediaDetail/MediaDetails.jsx
+++ src/components/MediaDetail/MediaDetails.jsx
@@ -20,66 +20,64 @@ function MediaDetails({ media }) {
   return (
     <>
       <div className={styles.detailsContainer}>
-        <div className={styles.info}>
-          {/* Trailer */}
-          {trailer_link && (
-            <a href={trailer_link} className={styles.trailerLink} target="_blank" rel="noopener noreferrer">
-              <span className={styles.watchTrailerText}>Watch Trailer</span>
-              <i className="fa-solid fa-play"></i>
-            </a>
+        {/* Trailer */}
+        {trailer_link && (
+          <a href={trailer_link} className={styles.trailerLink} target="_blank" rel="noopener noreferrer">
+            <span className={styles.watchTrailerText}>Watch Trailer</span>
+            <i className="fa-solid fa-play"></i>
+          </a>
+        )}
+        {/* Rating */}
+        <p className={styles.rating}>
+          {vote_average?.toFixed(1)} / 10 ({vote_count.toLocaleString()} Votes)
+        </p>
+        {/* Description */}
+        <p className={styles.description}>{overview}</p>
+        <div className={styles.meta}>
+          {/* Genres */}
+          {genres?.length > 0 && (
+            <p>
+              <strong>Genres:</strong> {genres.join(", ")}
+            </p>
           )}
-          {/* Rating */}
-          <p className={styles.rating}>
-            {vote_average?.toFixed(1)} / 10 ({vote_count.toLocaleString()} Votes)
-          </p>
-          {/* Description */}
-          <p className={styles.description}>{overview}</p>
-          <div className={styles.meta}>
-            {/* Genres */}
-            {genres?.length > 0 && (
-              <p>
-                <strong>Genres:</strong> {genres.join(", ")}
-              </p>
-            )}
-            {/* Country */}
-            {country && (
-              <p>
-                <strong>Country:</strong> {country}
-              </p>
-            )}
-            {/* Season Count */}
-            {number_of_seasons && (
-              <p>
-                <strong>Seasons:</strong> {number_of_seasons}
-              </p>
-            )}
-            {/* Duration */}
-            {runtime && (
-              <p>
-                <strong>Duration:</strong> {Math.floor(runtime / 60) > 0 ? `${Math.floor(runtime / 60)}h ` : ""}
-                {runtime % 60}m
-              </p>
-            )}
-            {/* Directors */}
-            {directors?.length > 0 && (
-              <p>
-                <strong>Directors:</strong> {directors.join(", ")}
-              </p>
-            )}
-            {/* Producers */}
-            {producers?.length > 0 && (
-              <p>
-                <strong>Producers:</strong> {producers.join(", ")}
-              </p>
-            )}
-            {/* Release date or first air date */}
-            {release_date || first_air_date ? (
-              <p>
-                <strong>Release Date:</strong> {release_date}
-                <strong>First Air Date:</strong> {first_air_date}
-              </p>
-            ) : null}
-          </div>
+          {/* Country */}
+          {country && (
+            <p>
+              <strong>Country:</strong> {country}
+            </p>
+          )}
+          {/* Season Count */}
+          {number_of_seasons && (
+            <p>
+              <strong>Seasons:</strong> {number_of_seasons}
+            </p>
+          )}
+          {/* Duration */}
+          {runtime && (
+            <p>
+              <strong>Duration:</strong> {Math.floor(runtime / 60) > 0 ? `${Math.floor(runtime / 60)}h ` : ""}
+              {runtime % 60}m
+            </p>
+          )}
+          {/* Directors */}
+          {directors?.length > 0 && (
+            <p>
+              <strong>Directors:</strong> {directors.join(", ")}
+            </p>
+          )}
+          {/* Producers */}
+          {producers?.length > 0 && (
+            <p>
+              <strong>Producers:</strong> {producers.join(", ")}
+            </p>
+          )}
+          {/* Release date or first air date */}
+          {release_date || first_air_date ? (
+            <p>
+              <strong>Release Date:</strong> {release_date}
+              <strong>First Air Date:</strong> {first_air_date}
+            </p>
+          ) : null}
         </div>
       </div>
     </>
